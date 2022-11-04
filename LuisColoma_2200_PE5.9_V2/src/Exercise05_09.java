c37a9049b32b9e5535077c0a0cfd971e74889dfc
lcolo <lcolo@10.0.0.22>
2022-11-04 10:35:47 -0500

    Push of of project done ICA

---
+++ LuisColoma_2200_PE5.9_V2/src/Exercise05_09.java
@@ -0,0 +1,47 @@
+/**************************************
+ * 
+ * @author Luis Coloma
+ *
+ */
+import java.util.Scanner;
+public class Exercise05_09 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the number of students: ");
+		int numberOfStudents = input.nextInt();
+		  // Make sure that score1 >= score2
+		
+		    String student1 = "";
+		    double score1 = Double.MIN_VALUE;
+		    String student2 = "";
+		    double score2 = Double.MIN_VALUE;
+		    
+		    for (int i = 0; i < numberOfStudents; i++) {
+		      System.out.print("Enter a student name: ");
+		      String student = input.next();
+
+		      System.out.print("Enter a student score: ");
+		      double score = input.nextDouble();
+
+		      if (score > score1) {
+		        student2 = student1;
+		        score2 = score1;
+
+		        student1 = student; 
+		        score1 = score;
+		      }
+		      else if (score > score2) {
+		        student2 = student; 
+		        score2 = score;
+		      }
+		    }
+
+		    System.out.println("Top two students:");
+		    System.out.println(student1 + "'s score is " + score1);
+		    System.out.println(student2 + "'s score is " + score2);
+		    
+	input.close();
+		}
+		
+	}
